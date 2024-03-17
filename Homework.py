9632b5e2efce207748a789cce155a76c0b49be07
ValeriiaFediuk <fediuk.valeriia14@gmail.com>
2024-03-17 22:13:23 +0200

    done

---
+++ Homework.py
@@ -0,0 +1,48 @@
+import time
+
+def find_coins_greedy(amount):
+    coins = [50, 25, 10, 5, 2, 1]
+    result = {}
+    for coin in coins:
+        if amount >= coin:
+            num_coins = amount // coin
+            result[coin] = num_coins
+            amount -= num_coins * coin
+    return result
+
+def find_min_coins(amount):
+    coins = [1, 2, 5, 10, 25, 50]
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+    coin_used = [-1] * (amount + 1)
+
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if i >= coin and dp[i - coin] + 1 < dp[i]:
+                dp[i] = dp[i - coin] + 1
+                coin_used[i] = coin
+
+    result = {}
+    while amount > 0:
+        coin = coin_used[amount]
+        if coin not in result:
+            result[coin] = 1
+        else:
+            result[coin] += 1
+        amount -= coin
+
+    return result
+
+
+amount = 214
+
+start_time = time.time()
+greedy_result = find_coins_greedy(amount)
+greedy_time = time.time() - start_time
+
+start_time = time.time()
+dp_result = find_min_coins(amount)
+dp_time = time.time() - start_time
+
+print("Жадібний алгоритм:", greedy_result, "Час виконання:", greedy_time)
+print("Алгоритм динамічного програмування:", dp_result, "Час виконання:", dp_time)
